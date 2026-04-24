89bc418a1c09ab3de536ee38dfc40ed7ac1f340e
Heejoo <sometimewillbe@gmail.com>
2026-04-25 02:06:12 +0900

    fix logo size and multiply blend

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 export const Logo = () => {
   return (
-    <div className="flex items-center h-20 md:h-28 lg:h-[110px] transition-all duration-300">
+    <div className="flex items-center h-28 md:h-36 lg:h-[130px] transition-all duration-300">
       <img
         src="/logo.png"
         alt="나란히"
-        className="h-full w-auto object-contain bg-transparent"
+        className="h-full w-auto object-contain"
         style={{
           imageRendering: '-webkit-optimize-contrast',
-          transform: 'translateZ(0) scale(1)'
+          transform: 'translateZ(0) scale(1)',
+          mixBlendMode: 'multiply'
         } as any}
       />
     </div>
