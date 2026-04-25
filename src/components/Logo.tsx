c71d805911c79763a2b6472bc9f739a961c68a16
Heejoo <sometimewillbe@gmail.com>
2026-04-25 17:16:25 +0900

    fix: 로고 사이즈 복구 및 배경 투명화

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 export const Logo = () => {
   return (
-    // 모바일(h-22)과 PC(md:h-28) 사이즈를 확실히 키웠습니다.
     <div className="flex items-center h-22 md:h-28 lg:h-[110px] transition-all duration-300">
       <img
         src="/logo.png"
@@ -11,8 +10,7 @@ export const Logo = () => {
         style={{
           imageRendering: '-webkit-optimize-contrast',
           transform: 'translateZ(0) scale(1)',
-          // 로고 이미지의 흰색 배경을 투명하게 만들어주는 설정입니다.
-          mixBlendMode: 'multiply' 
+          mixBlendMode: 'multiply' // 이 줄과 바로 윗줄 끝의 쉼표가 핵심입니다!
         } as any}
       />
     </div>
