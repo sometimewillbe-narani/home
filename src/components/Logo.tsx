62ab4f140a093a942d8776621ae7a0e09918121e
Heejoo <sometimewillbe@gmail.com>
2026-04-26 23:37:30 +0900

    fix: 링크 미리보기 및 사이트 로고 파일명 업데이트

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ export const Logo = () => {
   return (
     <div className="flex items-center h-22 md:h-28 lg:h-[110px] transition-all duration-300">
       <img
-        src="/logo.png"
+        src="/logo-new.png"
         alt="나란히"
         className="h-full w-auto object-contain"
         style={{
